3c1f1438c46a3bd8bada22d0e8d3429c6b96eb11
harryguo <harryguo@tencent.com>
2017-07-17 11:17:58 +0800

    添加字符串

--- app/src/main/kotlin/MainActivity.kt
+++ app/src/main/kotlin/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tv.text = "Hello MyKotlin"
+        tv.text = "Hello MyKotlin harryguo"
         tv.textSize = 20.0f
     }
 }
